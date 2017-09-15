e6973329c84d07d04bd964b339800800b3c18528
Michael Mason <TeaUponTweed@gmail.com>
2017-09-14 21:39:45 -0600

    wip

--- make_transaction.py
+++ make_transaction.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import sys
+import sys, os
 
 from Crypto.Signature import PKCS1_PSS
 from Crypto.Hash import SHA
@@ -9,15 +9,19 @@ from Crypto import Random
 
 
 def main(recipient, amount):
-    private_key = RSA.importKey(open('~/.nc/ncid_rsa').read())
-    public_key = RSA.importKey(open('~/.nc/ncid_rsa.pub').read())
+    private_key = RSA.importKey(open(os.path.expanduser('~/.nc/ncid_rsa')).read())
+    public_key = RSA.importKey(open(os.path.expanduser('~/.nc/ncid_rsa.pub')).read())
     h = SHA.new()
-    h.update(recipient)
-    h.update(str(amount).encode('ascii'))
+    h.update(recipient.encode('UTF-8'))
+    h.update(str(amount).encode('UTF-8'))
     signer = PKCS1_PSS.new(private_key)
     signature = signer.sign(h)
-    print(f'{recipient} {amount} {public_key} {signature}')
+    # import ipdb; ipdb.set_trace()
+    # sys.stdout.write()
+    # sys.stdout.write(hex(' '))
+    # sys.stdout.buffer.write(' '.encode('UTF-8'))
+    print(recipient + amount + public_key.exportKey('DER') + signature)
 
 if __name__ == '__main__':
-    recipient, amount = sys.argv
-    main(recipient, float(amount))
+    _, recipient, amount = sys.argv
+    main(recipient, amount)
